2ff1ff99bdcbc2fcd15a11a0acdba5a9b998da22
Alexz1008 <alexz1012@yahoo.com>
2019-12-03 21:04:10 -0800

    Finish program 2

--- prog123_tb.sv
+++ prog123_tb.sv
@@ -15,7 +15,9 @@ logic[15:0] d1_out[15];          // orig messages w/ parity inserted
 // program 2-specific variables
 logic[11:1] d2_in[15];           // use to generate data
 logic[15:0] d2_good[15];         // d2_in w/ parity
-logic[ 3:0] flip[15];            // position of corruption bit
+logic[ 3:0] flip[15];            // position of first corruption bit
+logic[ 5:0] flip2[15];           // position of possible second corruption bit
+logic[15:0] d2_bad1[15];         // possibly corrupt message w/ parity
 logic[15:0] d2_bad[15];          // possibly corrupt messages w/ parity
 logic       s16, s8, s4, s2, s1; // parity generated from data of d_bad
 logic[ 3:0] err;                 // bitwise XOR of p* and s* as 4-bit vector        
@@ -42,16 +44,13 @@ initial begin
 // rename "dm1" and/or "core" if you used different names for these
     DUT.data_mem1.core[2*i+1]  = {5'b0,d1_in[i][11:9]};
     DUT.data_mem1.core[2*i]    =       d1_in[i][ 8:1];
-    $writeb  (DUT.data_mem1.core[2*i+1]);
-    $displayb(DUT.data_mem1.core[2*i]);
-    $display();
   end
   #10ns reset = 1'b0;
   #10ns req   = 1'b1;          // pulse request to DUT
   #10ns req   = 1'b0;
   wait(ack);                   // wait for ack from DUT
 // generate parity for each message; display result and that of DUT
-  $display("finish program 1");
+  $display("start program 1");
   $display();
   for(int i=0;i<15;i++) begin
     p8 = ^d1_in[i][11:5];
@@ -70,8 +69,7 @@ initial begin
   end
 
 // program 2
-// generate parity from random 11-bit messages
-  $display("start program 2"); 
+// generate parity from random 11-bit messages 
   for(int i=0; i<15; i++) begin
 	d2_in[i] = $random;
     p8 = ^d2_in[i][11:5];
@@ -80,8 +78,12 @@ initial begin
     p1 = d2_in[i][11]^d2_in[i][ 9]^d2_in[i][7]^d2_in[i][5]^d2_in[i][4]^d2_in[i][2]^d2_in[i][1];
     p16 = ^d2_in[i]^p8^p4^p2^p1;
     d2_good[i] = {d2_in[i][11:5],p8,d2_in[i][4:2],p4,d2_in[i][1],p2,p1,p16};
+// flip one bit
     flip[i] = $random;
-    d2_bad[i] = d2_good[i] ^ (1'b1<<flip[i]);
+    d2_bad1[i] = d2_good[i] ^ (1'b1<<flip[i]);
+// flip second bit about 25% of the time (flip2<16)
+    flip2[i] = $random;
+	d2_bad[i] = d2_bad1[i] ^ (1'b1<<flip2[i]);
 	DUT.data_mem1.core[65+2*i] = {d2_bad[i][15:8]};
     DUT.data_mem1.core[64+2*i] = {d2_bad[i][ 7:0]};
   end
@@ -89,13 +91,25 @@ initial begin
   #10ns req   = 0;
   wait(ack);
   $display();
-  $display("finish program 2");
+  $display("start program 2");
   $display();
   for(int i=0; i<15; i++) begin
-    $displayb(d2_bad[i]);
-    $displayb({5'b0,d2_in[i]});
-    $writeb  (DUT.data_mem1.core[95+2*i]);
-    $displayb(DUT.data_mem1.core[94+2*i]);  
+    $displayb(d2_bad1[i][15:8], "_", d2_bad1[i][7:0]);
+    $displayb({5'b0,d2_in[i][11:9]}, "_", d2_in[i][8:1]);
+    $writeb  (DUT.data_mem1.core[95+2*i], "_");
+    $displayb(DUT.data_mem1.core[94+2*i]);
+    if((flip2[i][5:4]==0)&&(flip2[i][3:0]!=flip[i])) begin
+	  $display("double error injected here");
+	  if((DUT.data_mem1.core[95+2*i][7]==1'b0) || (DUT.data_mem1.core[95+2*i][7]===1'bx))
+        $display("missed the double error");
+    end
+                    
+    else if({5'b0,d2_in[i]}=={DUT.data_mem1.core[95+2*i],DUT.data_mem1.core[94+2*i]})
+	  $display("we have a match!");
+	else
+      $display("erroneous output");
+	$displayb(flip2[i],,flip[i]);
+	$displayb({5'b0,d1_in[i]},,{DUT.data_mem1.core[95+2*i],DUT.data_mem1.core[94+2*i]});
 	$display();
   end
 
